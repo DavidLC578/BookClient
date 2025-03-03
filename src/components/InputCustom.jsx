b702ad723a602644b7c8193adf23df7d40a5cf7a
DavidLC578 <davidlatt2002@gmail.com>
2025-03-02 20:00:09 -0500

    fix: value to input

--- src/components/InputCustom.jsx
+++ src/components/InputCustom.jsx
@@ -1,4 +1,4 @@
-function InputCustom({ label, type = "text", name, register }) {
+function InputCustom({ label, type = "text", name, register, value }) {
     return (
         <div className="flex flex-col">
             <label className="text-gray-300 mb-1 font-semibold" htmlFor={name}>
@@ -7,6 +7,7 @@ function InputCustom({ label, type = "text", name, register }) {
             {type === "textarea" ? (
                 <textarea
                     name={name}
+                    value={value}
                     className="bg-neutral-700 font-semibold text-white px-3 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
                     {...register(name)}
                 />
@@ -14,6 +15,7 @@ function InputCustom({ label, type = "text", name, register }) {
                 <input
                     type={type}
                     name={name}
+                    value={value}
                     className="bg-neutral-700 font-semibold text-white px-3 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
                     {...register(name)}
                 />
